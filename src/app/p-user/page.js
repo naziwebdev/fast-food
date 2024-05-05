912d887fbd6d28985a22142b7aa9334e2b07b428
nazi <nazipaeez777@gmail.com>
2024-05-05 18:58:03 +0330

    Do Dynamically box data

--- src/app/p-user/page.js
+++ src/app/p-user/page.js
@@ -3,15 +3,28 @@ import UserPanelLayout from "@/components/layouts/UserPanelLayout";
 import Box from "@/components/templates/P-user/Index/Box";
 import Tickets from "@/components/templates/P-user/Index/Tickets";
 import Orders from "@/components/templates/P-user/Index/Orders";
-export default function Index() {
+import connectTodb from "@/configs/db";
+import ticketModel from '@/models/ticket'
+import commentModel from '@/models/comment'
+import wishlistModel from '@/models/wishlist'
+import { authUser } from "@/utils/serverHelper";
+
+
+export default async function Index() {
+
+  const user = await authUser()
+  const tickets = await ticketModel.find({user:user._id})
+  const comments = await commentModel.find({user:user._id})
+  const wishlists = await wishlistModel.find({user:user._id})
+
   return (
     <UserPanelLayout>
       <div className={styles.index_wrapper}>
         <div className={styles.box_container}>
-          <Box  title={'مجموع تیکت ها'} count={10}/>
-          <Box  title={'مجموع کامنت ها'} count={0}/>
+          <Box  title={'مجموع تیکت ها'} count={tickets.length}/>
+          <Box  title={'مجموع کامنت ها'} count={comments.length}/>
           <Box title={'مجموع سفارش ها'} count={20}/>
-          <Box title={'مجموع علاقه مندی ها'} count={5}/>
+          <Box title={'مجموع علاقه مندی ها'} count={wishlists.length}/>
         </div>
         <div className={styles.user_details}>
           <Orders/>
