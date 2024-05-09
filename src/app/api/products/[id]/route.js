20425d4b43258ffee1d3fb887db861be1da68a2a
nazi <nazipaeez777@gmail.com>
2024-05-09 23:04:18 +0330

    Develope api for delete product

---
+++ src/app/api/products/[id]/route.js
@@ -0,0 +1,28 @@
+import connectTodb from "@/configs/db";
+import productModel from "@/models/product";
+import { isValidObjectId } from "mongoose";
+import { authAdmin } from "@/utils/serverHelper";
+
+export async function DELETE(req, { params }) {
+  try {
+    const isAdmin = await authAdmin();
+
+    if (!isAdmin) {
+      return Response.json({message:'this api is protected'},{status:401})
+    }
+    connectTodb();
+
+    if (!isValidObjectId(params.id)) {
+      return Response.json({ message: "id is not valid" }, { status: 422 });
+    }
+
+    await productModel.findOneAndDelete({ _id: params.id });
+
+    return Response.json(
+      { message: "comment removed successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return Response.json({ message: error }, { status: 500 });
+  }
+}
