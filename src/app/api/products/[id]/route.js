e099d63f8c3d64de45c9722891595fd3cef80b9a
nazi <nazipaeez777@gmail.com>
2024-05-11 22:45:05 +0330

    Develope edit product api & do logic it in client

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -1,22 +1,107 @@
 import connectTodb from "@/configs/db";
 import productModel from "@/models/product";
+import productValidation from "@/validations/detailsProduct";
 import { isValidObjectId } from "mongoose";
 import { authAdmin } from "@/utils/serverHelper";
+import { writeFile } from "fs/promises";
+import path from "path";
+
+export async function PUT(req, { params }) {
+  try {
+    const isAdmin = await authAdmin();
+
+    if (!isAdmin) {
+      return Response.json(
+        { message: "this api is protected" },
+        { status: 401 }
+      );
+    }
+    connectTodb();
+
+    if (!isValidObjectId(params.id)) {
+      return Response.json({ message: "id is not valid" }, { status: 422 });
+    }
+
+    const formData = await req.formData();
+    const title = formData.get("title");
+    const price = formData.get("price");
+    const tags = formData.get("tags");
+    const description = formData.get("description");
+    const countAvailable = formData.get("countAvailable");
+    const weight = formData.get("weight");
+    const materials = formData.get("materials");
+    const tast = formData.get("tast");
+    const size = formData.get("size");
+    const img = formData.get("img");
 
 
-// export async function PUT (req,{params}){
+    await productValidation
+      .validate({
+        title,
+        price,
+        tags: tags.split(","),
+        description,
+        countAvailable,
+        weight,
+        materials: materials.split(","),
+        tast,
+        size,
+        img,
+      })
+      .catch((error) => {
+        error.statusCode = 400;
+        throw error;
+      });
 
-// }
+    const buffer = Buffer.from(await img.arrayBuffer());
+
+    const filename = new Date().getTime() + img.name;
 
 
+    const pathFile = path.join(process.cwd(), "public/uploads/" + filename);
+    
 
+    await writeFile(pathFile, buffer);
+
+    await productModel.findOneAndUpdate(
+      { _id:params.id },
+      {
+        title,
+        price,
+        tags: tags.split(","),
+        description,
+        countAvailable,
+        weight,
+        materials: materials.split(","),
+        tast,
+        size,
+        img: `http://localhost:3000/uploads/${filename}`,
+      }
+    );
+
+   
+
+    return Response.json(
+      { message: "product updated successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return Response.json(
+      { message: error },
+      { status: error.statusCode || 500 }
+    );
+  }
+}
 
 export async function DELETE(req, { params }) {
   try {
     const isAdmin = await authAdmin();
 
     if (!isAdmin) {
-      return Response.json({message:'this api is protected'},{status:401})
+      return Response.json(
+        { message: "this api is protected" },
+        { status: 401 }
+      );
     }
     connectTodb();
 
--- src/components/templates/P-admin/Products/DataTable.js
+++ src/components/templates/P-admin/Products/DataTable.js
@@ -4,15 +4,107 @@ import swal from "sweetalert";
 import { useRouter } from "next/navigation";
 import Modal from "@/components/modules/Modal/Modal";
 import { useState } from "react";
+import productValidation from "@/validations/detailsProduct";
 import Image from "next/image";
 
 export default function DataTable({ products }) {
   const router = useRouter();
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState(0);
+  const [tags, setTags] = useState([]);
+  const [description, setDescription] = useState("");
+  const [countAvailable, setCountAvailable] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [materials, setMaterals] = useState([]);
+  const [tast, setTast] = useState("");
+  const [size, setSize] = useState(0);
+  const [img, setImg] = useState({});
+  const [productID,setProductID] = useState("")
 
   const [showModal, setShowModal] = useState(false);
 
   const closeModal = () => {
     setShowModal(false);
+  };
+
+
+  const editHandler = (product) => {
+    setShowModal(true)
+    setTitle(product.title)
+    setPrice(product.price)
+    setTags(product.tags)
+    setDescription(product.description)
+    setCountAvailable(product.countAvailable)
+    setWeight(product.weight)
+    setMaterals(product.materials)
+    setTast(product.tast)
+    setSize(product.size)
+    setProductID(product._id)
+
+  }
+
+  const editProduct = async (event) => {
+ 
+    event.preventDefault();
+    setShowModal(false)
+    const formData = new FormData();
+    
+    formData.append("title", title);
+    formData.append("price", price);
+    formData.append("tags", tags);
+    formData.append("description", description);
+    formData.append("countAvailable", countAvailable);
+    formData.append("weight", weight);
+    formData.append("materials", materials);
+    formData.append("tast", tast);
+    formData.append("size", size);
+    formData.append("img", img);
+
+    try {
+      await productValidation.validate({
+        title,
+        price,
+        tags,
+        description,
+        countAvailable,
+        weight,
+        materials,
+        tast,
+        size,
+        img,
+      });
+    } catch (err) {
+      return swal({
+        title: err,
+        icon: "error",
+        buttons: "تلاش دوباره",
+      });
+    }
+
+    const res = await fetch(`/api/products/${productID}`, {
+      method: "PUT",
+      body: formData,
+    });
+
+    if (res.status === 200) {
+
+      swal({
+        title: " با موفقیت ثبت شد",
+        icon: "success",
+        buttons: "بستن",
+      }).then((value) => {
+        if (value) {
+          router.refresh();
+        }
+      });
+    } else {
+      swal({
+        title: await res.status,
+        icon: "error",
+        buttons: "تلاش دوباره",
+      });
+      console.log(await res.json());
+    }
   };
 
   const removeProduct = async (productID) => {
@@ -70,8 +162,13 @@ export default function DataTable({ products }) {
             <tr key={item._id} className={styles.table_row}>
               <td className={styles.table_col}>{index + 1}</td>
               <td>
-                <img src={item?.img} width={70} height={60} alt="product"
-                className={styles.table_img}/>
+                <img
+                  src={item?.img}
+                  width={90}
+                  height={60}
+                  alt="product"
+                  className={styles.table_img}
+                />
               </td>
               <td>{item.title}</td>
               <td>{item.price.toLocaleString("fa-IR")}</td>
@@ -79,7 +176,7 @@ export default function DataTable({ products }) {
               <td>{item.score}</td>
               <td>
                 <button
-                  onClick={() => setShowModal(true)}
+                  onClick={() => editHandler(item)}
                   className={`${styles.btn} ${styles.seen_btn}`}
                 >
                   ویرایش
@@ -97,7 +194,114 @@ export default function DataTable({ products }) {
           ))}
         </tbody>
       </table>
-      {showModal && <Modal onClose={closeModal}>hi</Modal>}
+      {showModal && (
+        <Modal onClose={closeModal}>
+          <form className={styles.form}>
+            <div className={styles.input_wrapper}>
+              <label htmlFor="title">عنوان </label>
+              <input
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+                id="title"
+                type="text"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_wrapper}>
+              <label htmlFor="price">قیمت</label>
+              <input
+                value={price}
+                onChange={(event) => setPrice(event.target.value)}
+                id="price"
+                type="number"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_wrapper}>
+              <label htmlFor="tags">تگ ها </label>
+              <input
+                value={tags}
+                onChange={(event) => setTags(event.target.value.split(","))}
+                id="tags"
+                type="text"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_wrapper}>
+              <label htmlFor="description">توضیحات</label>
+              <textarea
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+                id="description"
+                className={styles.textarea}
+              ></textarea>
+            </div>
+            <div className={styles.input_wrapper}>
+              <label htmlFor="countAvailable">موجودی </label>
+              <input
+                value={countAvailable}
+                onChange={(event) => setCountAvailable(event.target.value)}
+                id="countAvailable"
+                type="number"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_wrapper}>
+              <label htmlFor="weight">وزن </label>
+              <input
+                value={weight}
+                onChange={(event) => setWeight(event.target.value)}
+                id="weight"
+                type="number"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_wrapper}>
+              <label htmlFor="materials">مواد تشکیل دهنده</label>
+              <input
+                value={materials}
+                onChange={(event) => setMaterals(event.target.value.split(","))}
+                id="materials"
+                type="text"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_wrapper}>
+              <label htmlFor="tast">مزه </label>
+              <input
+                value={tast}
+                onChange={(event) => setTast(event.target.value)}
+                id="tast"
+                type="text"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_wrapper}>
+              <label htmlFor="size">اندازه</label>
+              <input
+                value={size}
+                onChange={(event) => setSize(event.target.value)}
+                id="size"
+                type="number"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_wrapper}>
+              <label htmlFor="img">عکس </label>
+              <input
+                onChange={(event) => setImg(event.target.files[0])}
+                id="img"
+                type="file"
+                className={styles.input}
+              />
+            </div>
+
+            <button onClick={editProduct} className={styles.form_btn}>
+              آپدیت
+            </button>
+          </form>
+        </Modal>
+      )}
     </div>
   );
 }
