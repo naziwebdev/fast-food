7d429865e9472f340557e7814f87d77e9c0b55ae
nazi <nazipaeez777@gmail.com>
2024-05-11 23:02:52 +0330

    Do logic of edit user in p-admin

--- src/components/templates/P-admin/Users/DataTable.js
+++ src/components/templates/P-admin/Users/DataTable.js
@@ -2,10 +2,30 @@
 import styles from "./DataTable.module.css";
 import swal from "sweetalert";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Modal from "@/components/modules/Modal/Modal";
+import UserValidator from "@/validations/editUser";
 
 export default function DataTable({ users }) {
-  
   const router = useRouter();
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [userID, setUserID] = useState("");
+
+  const [showModal, setShowModal] = useState(false);
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const editHandler = (user) => {
+    setShowModal(true);
+    setName(user.name);
+    setPhone(user.phone);
+    setEmail(user?.email);
+    setUserID(user._id);
+  };
 
   const removeUser = async (userID) => {
     swal({
@@ -27,7 +47,7 @@ export default function DataTable({ users }) {
             buttons: "بستن",
           }).then((value) => {
             if (value) {
-             router.refresh()
+              router.refresh();
             }
           });
         } else {
@@ -65,7 +85,7 @@ export default function DataTable({ users }) {
             buttons: "بستن",
           }).then((value) => {
             if (value) {
-              router.refresh()
+              router.refresh();
             }
           });
         } else {
@@ -79,8 +99,56 @@ export default function DataTable({ users }) {
       }
     });
   };
+  const editUser = async (event) => {
+    event.preventDefault();
+    setShowModal(false);
+    const newUser = {
+      name,
+      phone,
+      email,
+    };
 
-  const banUser = async (phone,email) => {
+    try {
+      await UserValidator.validate(newUser);
+    } catch (err) {
+      return swal({
+        title: err,
+        icon: "error",
+        buttons: "تلاش دوباره",
+      });
+    }
+
+    const res = await fetch(`/api/user/${userID}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    });
+
+    if (res.status === 200) {
+      (await res).json();
+
+      swal({
+        title: " با موفقیت ثبت شد",
+        icon: "success",
+        buttons: "بستن",
+      }).then((value) => {
+        if (value) {
+          router.refresh();
+        }
+      });
+    } else {
+      swal({
+        title: "  با شکست مواجه شد",
+        icon: "error",
+        buttons: "تلاش دوباره",
+      });
+      console.log(await res);
+    }
+  };
+
+  const banUser = async (phone, email) => {
     swal({
       title: "آیا از بن اطمینان دارید",
       icon: "warning",
@@ -92,7 +160,7 @@ export default function DataTable({ users }) {
           headers: {
             "content-type": "application/json",
           },
-          body: JSON.stringify({ phone ,email }),
+          body: JSON.stringify({ phone, email }),
         });
 
         if (res.status === 200) {
@@ -104,7 +172,7 @@ export default function DataTable({ users }) {
             buttons: "بستن",
           }).then((value) => {
             if (value) {
-              router.refresh()
+              router.refresh();
             }
           });
         } else {
@@ -142,7 +210,10 @@ export default function DataTable({ users }) {
               <td>{item.phone}</td>
               <td>{item.role === "ADMIN" ? "مدیر" : "کاربر"}</td>
               <td>
-                <button className={`${styles.btn} ${styles.edit_btn}`}>
+                <button
+                  onClick={() => editHandler(item)}
+                  className={`${styles.btn} ${styles.edit_btn}`}
+                >
                   ویرایش
                 </button>
               </td>
@@ -164,7 +235,7 @@ export default function DataTable({ users }) {
               </td>
               <td>
                 <button
-                  onClick={() => banUser(item.phone,item?.email)}
+                  onClick={() => banUser(item.phone, item?.email)}
                   className={`${styles.btn} ${styles.ban_btn}`}
                 >
                   بن
@@ -174,6 +245,46 @@ export default function DataTable({ users }) {
           ))}
         </tbody>
       </table>
+      {showModal && (
+        <Modal onClose={closeModal}>
+          <form className={styles.form}>
+            <div className={styles.input_wrapper}>
+              <label htmlFor="name">نام و نام خانوادگی </label>
+              <input
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+                id="name"
+                type="text"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_wrapper}>
+              <label htmlFor="phone">شماره همراه</label>
+              <input
+                value={phone}
+                onChange={(event) => setPhone(event.target.value)}
+                id="phone"
+                type="text"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_wrapper}>
+              <label htmlFor="email"> ایمیل </label>
+              <input
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                id="email"
+                type="email"
+                className={styles.input}
+              />
+            </div>
+
+            <button onClick={editUser} className={styles.form_btn}>
+              آپدیت
+            </button>
+          </form>
+        </Modal>
+      )}
     </div>
   );
 }
