96a46363e2baeb5e9d98d374e5f0ca35bb2a85cc
nazi <nazipaeez777@gmail.com>
2024-05-11 23:59:35 +0330

    Do logic of add product in product card

--- src/components/modules/ProductCard/ProductCard.js
+++ src/components/modules/ProductCard/ProductCard.js
@@ -1,3 +1,5 @@
+"use client"
+
 import styles from "./ProductCard.module.css";
 import Image from "next/image";
 import { FaRegStar, FaStar } from "react-icons/fa";
@@ -5,10 +7,70 @@ import { SlBasket } from "react-icons/sl";
 import Link from "next/link";
 import FavoriteBtn from "@/components/modules/FavoriteBtn/FavoriteBtn";
 import RemoveFavoriteBtn from "../RemoveFavoriteBtn/RemoveFavoriteBtn";
-
+import swal from "sweetalert";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function ProductCard({ remove ,isfull, product}) {
+  const [counter, setCounter] = useState(1);
+  const router = useRouter()
+  const addBasket = () => {
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+    if (cart.length) {
+      const isInCart = cart.some((item) => item.id == product._id);
+
+      if (isInCart) {
+        cart.forEach((item) => {
+          if (item.id == product._id) {
+            item.count += counter;
+          }
+        });
+      } else {
+        const mainProduct= {
+          id: product._id,
+          title: product.title,
+          count: counter,
+          price: product.price,
+        };
+
+        cart.push(mainProduct);
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+        swal({
+          title: "محصول با موفقیت به سبد خرید افزوده شد",
+          icon: "success",
+          buttons: "بستن",
+        });
+      }
+      localStorage.setItem("cart", JSON.stringify(cart));
+      swal({
+        title: "محصول با موفقیت به سبد خرید افزوده شد",
+        icon: "success",
+        buttons: "بستن",
+      });
+    } else {
+      const mainProduct = {
+        id: product._id,
+        title: product.title,
+        count: counter,
+        price: product.price,
+      };
+
+      cart.push(mainProduct);
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+      swal({
+        title: "محصول با موفقیت به سبد خرید افزوده شد",
+        icon: "success",
+        buttons: "بستن",
+      }).then((value) => {
+        if(value){
+            router.refresh()
+        }
+      });
+    }
+  };
   return (
     <div className={`${styles.card} ${isfull && styles.full_card} `}>
       <div className={styles.card_head}>
@@ -25,7 +87,7 @@ export default function ProductCard({ remove ,isfull, product}) {
             <FavoriteBtn product={product._id} />
   }
           </div>
-          <div className={styles.card_addBtn}>
+          <div onClick={addBasket}  className={styles.card_addBtn}>
             <SlBasket className={styles.card_icon} />
             <p className={styles.card_addText}>افزودن به سبد خرید</p>
           </div>
